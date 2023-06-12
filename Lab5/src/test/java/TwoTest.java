47f05479feeca8334b830abad12237a7b1b018c6
Djeck22 <126654065+Djeck22@users.noreply.github.com>
2023-06-12 14:06:46 +0300

    Add files via upload

---
+++ Lab5/src/test/java/TwoTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import org.junit.Test;
+
+public class TwoTest extends AbstractTest{
+
+    @Test
+    public void test2()  throws InterruptedException {
+
+
+
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10).until(ExpectedConditions.urlContains("https://www.livejournal.com")));
+        Assertions.assertTrue(getDriver().getTitle().contains("Живой журнал"), "страница входа не доступна");
+
+
+        Actions search = new Actions(getDriver());
+
+        search.click(getDriver().findElement(By.xpath("//*[@id=\\\"js\\\"]/body/div[1]/header/div[1]/nav[2]/ul/li[1]/div/form/button/span")))
+                .findElement(By.xpath("/html/body/div[1]/header/div[1]/nav[2]/ul/li[1]/div/form/div/div/input"))
+                .sendKeys("TestGeekBrains")
+                .click(getDriver().findElement(By.xpath("//*[@id=\\\"js\\\"]/body/div[1]/header/div[1]/nav[2]/ul/li[1]/div/form/button/span")))
+                .build()
+                .perform();
+        Thread.sleep(1000);
+
+    }
+}
